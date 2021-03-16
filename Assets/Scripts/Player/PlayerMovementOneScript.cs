34af530b61ec4f300858f03bda144d23090ad555
Frankie-duke <frankiedukedaley@gmail.com>
2021-03-17 01:49:33 +1100

    I cant finish this tonight
    I've had 4 cups of coffee but it has done basically nothing, i am falling asleep at the keyboard, this sucks but at least i got the base movement PERFECT.
    
    Also each variable has a tooltip now, I'm gonna try to get this done when I wake up.

--- Assets/Scripts/Player/PlayerMovementOneScript.cs
+++ Assets/Scripts/Player/PlayerMovementOneScript.cs
@@ -17,50 +17,68 @@ public class PlayerMovementOneScript : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
 
-    [Tooltip("Note that all speeds are in meters per second")]
+    //Note that all speeds are in meters per second
     [Header("Speeds")]
-    public float CurrentSpeed = 6f; //How fast the player is currently moving
-    public float MaxBaseSpeed = 16f;
-    public float MidBaseSpeed;
-    public float MinBaseSpeed = 3f;
-    public float Acceleration = 4f; //how fast the player will reach max base speed from min base speed
-    public float Deceleration = 5f; //how fast the player will reach min base speed from buffed speed and how fast the player will reach idle from AMS with 0 input
-    public float JumpHeight;
-    public float InAirAcceleration; //this is for input movement
-    public float InAirDeceleration; //this too
-    public float AirControllBuffer;
+    [Tooltip("This is the speed the player moves at as far as the input is concerened || Default = 6f")]
+    public float CurrentSpeed = 6f; 
+    [Tooltip("This is the max speed the player can go to, they can only reach it by moving forward and sprinting || Default = 7f")]
+    public float MaxBaseSpeed = 7f;
+    [Tooltip("This is the VERY default speed, it is the speed the player moves at in all directions without sprinting || Default = 5f")]
+    public float MidBaseSpeed = 5f;
+    [Tooltip("This is the speed the player starts moving at and decelerates to. NOTE: If the currentSpeed somehow falls bellow this value the player can't move, I have built in saftey nets just in case || Default = 2f")]
+    public float MinBaseSpeed = 2f;
+    [Tooltip("Pretty self explanitory rly, this is how fast the player gets from one speed to another || Default = 4")]
+    public float Acceleration = 4f;
+    [Tooltip("The exact Opposite of Acceleration || Default = 5f")]
+    public float Deceleration = 5f; 
+    [Tooltip("How high (in meteres) the player jumps, they are a ninja so they can jump 11 meters high, shut up || Default = 11f")]
+    public float JumpHeight = 11f;
+    [Tooltip("Deceleration for Moving in the air, This is optional, the script does not need it but it might make the game better later || Default = 0f")]
+    public float InAirDeceleration = 0f; 
+    [Tooltip("This is the amount of buffer room the player can move once they are in the air, it is added to the MaxAirVelocity || Default = 0.3f")]
+    public float AirControllBuffer = 0.3f;
+    [Tooltip("This is multiplied to the InAirMovement to allow the player to be cool || Default = 5f")]
+    public float InAirControl;
 
     [Header("INPUT")]
+    [Tooltip("This is what the character controller looks to for the move direction, the move direction looks to the Direction normal for the direction, which it then transforms the direction from local space to world space, the moveDirection gets the input from InAirMovement while its in the air but still keeps the movement from the ground so the velocity carries over")]
     private Vector3 moveDirection = Vector3.zero;
+    [Tooltip("This vector3 gets input from the x and z axis and then normalizes it so the player can't diagonal run to go faster, the InAirMovement gets the input from here aswell as the MoveDirection vetor3")]
     private Vector3 directionNormal;
+    [Tooltip("When the player enters the air, the player still moves based on moveDirection but it doesnt get updated every frame like in moving state, when the code detects input it goes to this vector3 which is multiplied by InAirControll and then added or subtracted from the moveDirection Vector3 every second, this has the fluid feeling of velocity alteration. Based off of titanfall 2 and various 2d platformers")]
     private Vector3 InAirMove = Vector3.zero;
+    [Tooltip("The InAirMove gets added to move direction every second, this is the (soft) limit to how much velocity the player can build up in the air")]
     private Vector3 MaxAirVelocity;
     [Tooltip("The A-D input")]
     private float horizontal;
     [Tooltip("The W-S input")]
     private float vertical;
-    public float InAirControl;
-    public bool IsSprinting;
+    [Tooltip("Do I even need to explain this one?")]
+    private bool IsSprinting;
 
     [Header("Checks and physics")]
+    [Tooltip("Unity's in-built character controller has a 'check if grounded' function but it rarely works for me so i have the habit of making a custom check my self, this transform is just the location of the detection")]
     public Transform GroundCheck;
+    [Tooltip("Is the ground under the player with in the specified parameteres?")]
     public bool IsGrounded;
+    [Tooltip("Hmm yes the floor is made out of floor")]
     public LayerMask GroundMask;
     public LayerMask WallMask;
+    [Tooltip("This is the radius of the sphere cast that detects the ground, this has to be equal or greater than the character controllers radius || def = 0.4f")]
     public float groundDistance = 0.4f;
-    public float Gravity = 30.0f;
+    [Tooltip("There is a thing, known as gravity... do you believe it? IRL gravity is about 9.7 meteres a second, this is 25 meteres a second || def = 25f")]
+    public float Gravity = 25f;
+    [Tooltip("Issac newton can shut the fuck up")]
+    public float WallGravity = 10f;
+    [Tooltip("This one is purley for gravity, since im not a pleb who uses a rigidbody movement, I like to keep gravity and move direction seperate ")]
     private Vector3 _playerVelocity = Vector3.zero;
-    public bool CanDash;
 
     [Header("Debugging")]
+    [Tooltip("How fast the player is moving at ANY given time in meteres a second")]
     public float TotalVelocity;
+    [Tooltip("just gotta time the TotalVelocity by 3.6f and its in Kilometers an hour")]
     public float KPH;
-    public bool IsControllingAir;
-
-    public float MoveDirectionMagnitude;
-    public float DirectionNormalMagnitude;
-    public float InAirMoveMagnitude;
-    public float MaxAirVelocityMagnitude;
+   
    
    
     private void Start()
@@ -72,14 +90,6 @@ public class PlayerMovementOneScript : MonoBehaviour
     }
     private void Update()
     {
-        //DEBUG
-        //========================================================================================================================================================
-        MoveDirectionMagnitude = moveDirection.magnitude;
-        DirectionNormalMagnitude = directionNormal.magnitude;
-        InAirMoveMagnitude = InAirMove.magnitude;
-        MaxAirVelocityMagnitude = MaxAirVelocity.magnitude;
-        //========================================================================================================================================================
-        
         //INPUT and CHECKS
         //========================================================================================================================================================
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -92,7 +102,6 @@ public class PlayerMovementOneScript : MonoBehaviour
         if (IsGrounded)
         {
             MaxAirVelocity = transform.TransformDirection(new Vector3(Mathf.Abs(moveDirection.x) + AirControllBuffer, 0f, Mathf.Abs(moveDirection.z) + AirControllBuffer));
-            IsControllingAir = false;
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _playerVelocity.y = JumpHeight;
@@ -147,7 +156,6 @@ public class PlayerMovementOneScript : MonoBehaviour
     {
         while (state == State.Idle)
         {
-            CanDash = true;
             IsSprinting = false;
             if (CurrentSpeed > MinBaseSpeed)
             {
